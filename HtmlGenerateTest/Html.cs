b8f1a4ec9a6746ca2459923d01dc689f59ab5fb3
YasinDaeazz <122436989+YasinDaeazz@users.noreply.github.com>
2023-05-25 12:17:40 +0300

    init

---
+++ HtmlGenerateTest/Html.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HtmlGenerateTest
+{
+    public static class Html
+    {
+        public static string html = "";
+    }
+}
---
+++ HtmlGenerateTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using SelectPdf;
+using System;
+
+namespace HtmlGenerateTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string htmlContent = System.IO.File.ReadAllText("HTMLPage1.html");
+
+            HtmlToPdf converter = new HtmlToPdf();
+
+            PdfDocument doc = converter.ConvertHtmlString(htmlContent);
+
+            doc.Save("C:/Users/yasin/source/test.pdf");
+            doc.Close();
+        }
+    }
+}
